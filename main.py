826e8eece1d147363bbc8e7a15995f0419464af3
Aman Jha <anuragjhav51@gmail.com>
2026-05-11 00:11:22 +0530

    Refactor project structure by renaming backend files and consolidating imports

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routes import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Chatbot API running"
+    }
